74407826bbbb89def08d3f55f97971f4470f6d79
PULKITGOYAL18 <pulkitgoyal0103@gmail.com>
2026-08-07 20:27:15 +0530

    Update roleUtils.ts

--- src/utils/roleUtils.ts
+++ src/utils/roleUtils.ts
@@ -1,3 +1,7 @@
 export function getDashboardScreen(role: string): string {
   return `dashboard-${role.toLowerCase()}`
 }
+
+export function getHomeScreen(role: string): string {
+  return getDashboardScreen(role)
+}
